2b094d27d65bb800d7ca0667ff03f09eed59cd24
banobepascal <banobepascal@gmail.com>
2019-09-05 18:18:29 +0300

    [Finishes #168303768] Finished Landing page

---
+++ ui/main.js
@@ -0,0 +1,21 @@
+// Js for Hide and show modal
+document.getElementById('post-btn').addEventListener('click', 
+function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.bg-modal').style.display = 'none'; 
+});
+
+/////////////////////////////////////////////////////////
+
+// Js for hide and show search bar
+document.querySelector('#search-icon').addEventListener('click', 
+function(){
+    document.querySelector('.search').style.display = 'block'; 
+});
+
+///////////////////////////////////////////////////////
+
