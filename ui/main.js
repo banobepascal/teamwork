bacf8e58625ee3dd86f5771909bad0057abd8639
banobepascal <banobepascal@gmail.com>
2019-09-05 22:16:57 +0300

    adding modal

--- ui/main.js
+++ ui/main.js
@@ -1,45 +1,74 @@
 // Js for Hide and show modal
-document.getElementById('post-btn').addEventListener('click', 
-function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
+document.getElementById("post-btn").addEventListener("click", function() {
+  document.querySelector(".bg-modal").style.display = "flex";
 });
 
-document.querySelector('.close').addEventListener('click', 
-function(){
-    document.querySelector('.bg-modal').style.display = 'none'; 
+document.querySelector(".close").addEventListener("click", function() {
+  document.querySelector(".bg-modal").style.display = "none";
 });
 
 /////////////////////////////////////////////////////////
 
 // Js for hide and show search bar
-document.querySelector('#search-icon').addEventListener('click', 
-function(){
-    document.querySelector('.search').style.display = 'block'; 
+document.querySelector("#search-icon").addEventListener("click", function() {
+  document.querySelector(".search").style.display = "block";
 });
 
 ///////////////////////////////////////////////////////
 
 // Js for post validation
 
-const myPost = document.querySelector('#my-post');
-const topicInput = document.querySelector('#topicInput');
-const sayInput = document.querySelector('#sayInput');
-const topicError = document.querySelector('.topic-msg');
-const topicError1 = document.querySelector('.topic-error1');
-const topicError2 = document.querySelector('.topic-error2');
-const postBtn = document.querySelector('.post-button');
+const myPost = document.querySelector("#my-post");
+const topicInput = document.querySelector("#topicInput");
+const sayInput = document.querySelector("#sayInput");
+const topicError = document.querySelector(".topic-msg");
+const topicError1 = document.querySelector(".topic-error1");
+const modalInput = document.querySelector(".bg-modal");
+const postBtn = document.querySelector(".post-button");
+const heroPost = document.querySelector(".hero");
 
-myPost.addEventListener('submit', onSubmit);
+myPost.addEventListener("submit", onSubmit);
 
-function onSubmit(e){
-    e.preventDefault();
+function onSubmit(e) {
+  e.preventDefault();
 
-    if(topicInput.value === "" || sayInput.value === "") {
-        topicError.classList.add('msg');
-        topicError.innerHTML = 'Please fill in Topic and Your Say';
-        setTimeout(() => topicError.remove(), 3000);
-    } else {
-        console.log('success');
-    }
+  if (topicInput.value === "" || sayInput.value === "") {
+    topicError.classList.add("msg-error");
+    topicError.innerHTML = "Please fill in Topic and Your Say";
+    setTimeout(() => topicError.remove(), 3000);
+  } else if (topicInput.value.length < 5 || sayInput.value.length < 20) {
+    topicError.classList.add("msg-error");
+    topicError.innerHTML = "Minimum 3 characters for Topic and 20 for Your Say";
+    setTimeout(() => topicError.remove(), 3000);
+  } else {
+    const post = document.createElement("div");
+    post.className = "hero-content-article";
 
+    post.innerHTML = ` <div class="hero-content-article">
+        <div class="name-time">
+          <p id="username-article">Username</p>
+          <p id="time-article">post time</p>
+        </div>
+        <h4>${topicInput.value}</h4>
+        <p>
+        ${sayInput.value}
+       </p>
+        <ul>
+          <li>
+            <img src="svgs/comment.svg" alt="" id="comment" />
+          </li>
+          <li>
+            <img src="svgs/thumbs-down.svg" alt="" id="thumb-down" />
+          </li>
+        </ul>
+      </div>`;
+
+    topicError.classList.add("msg");
+    topicError.innerHTML = "Your Post was sent successfully";
+    heroPost.appendChild(post);
+    // setTimeout(() => modalInput.remove(), 3000);
+    // clear fields
+    topicInput.value = "";
+    sayInput.value = "";
+  }
 }
