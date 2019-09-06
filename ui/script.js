d96a399063834ff5c8f7c61ff003c565a7a08583
banobepascal <banobepascal@gmail.com>
2019-09-06 22:36:03 +0300

    [Starts #168328572] view modal working but has some issues

---
+++ ui/script.js
@@ -0,0 +1,7 @@
+document.querySelector(".post-btn").addEventListener("click", function() {
+    document.querySelector(".bg-modal").style.display = "flex";
+  });
+  
+  document.querySelector(".close").addEventListener("click", function() {
+    document.querySelector(".bg-modal").style.display = "none";
+  });
