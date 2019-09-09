19d6d5450d7ed81561e597796b96547d0e463262
banobepascal <banobepascal@gmail.com>
2019-09-10 02:58:32 +0300

    [Finishes #168385977] Created confirm modal for article delete

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,34 +1,71 @@
 // Js for Hide and show edit modal
-  // Get DOM Elements
-  const editModal = document.querySelector('.bg-edit');
-  const editBtn = document.querySelector('#edit-btn');
-  const closeEdit = document.querySelector('.close-edit');
-  
-  // Events
-  editBtn.addEventListener('click', openEdit);
-  closeEdit.addEventListener('click', closeEditModal);
-  window.addEventListener('click', outsideClose);
-  
-  // Open
-  function openEdit() {
-    editModal.style.display = 'flex';
-  }
-  
-  // Close
-  function closeEditModal() {
-    editModal.style.display = 'none';
-  }
-  
-  // Close If Outside Click
-  function outsideClose(e) {
-    if (e.target == editModal) {
-      editModal.style.display = 'none';
-    }
-  }
+// Get DOM Elements
+const editModal = document.querySelector(".bg-edit");
+const editBtn = document.querySelector("#edit-btn");
+const closeEdit = document.querySelector(".close-edit");
 
+// Events
+editBtn.addEventListener("click", openEdit);
+closeEdit.addEventListener("click", closeEditModal);
+window.addEventListener("click", outsideClose);
 
-  const onDelete = () => {
-    if (prompt('Are you sure to delete this record ?')) {
-        document.querySelector(".hero-content-article").outerHTML = "";
-    }
+// Open
+function openEdit() {
+  editModal.style.display = "flex";
 }
+
+// Close
+function closeEditModal() {
+  editModal.style.display = "none";
+}
+
+// Close If Outside Click
+function outsideClose(e) {
+  if (e.target == editModal) {
+    editModal.style.display = "none";
+  }
+}
+
+//Delete confirm modal
+const onDelete = () => {
+  const deleteBtn = document.querySelector("#delete-btn");
+  const confirmBox = document.querySelector(".confirm");
+  const closeDelete = document.querySelector(".confirm__close");
+  const btnOk = document.querySelector(".confirm__button--ok");
+  const btnCancel = document.querySelector(".confirm__button--cancel");
+
+  // delete events
+  deleteBtn.addEventListener("click", openDelete);
+  closeDelete.addEventListener("click", closeDeleteModal);
+  btnOk.addEventListener("click", deleteArticle);
+  btnCancel.addEventListener("click", cancelDelete);
+  window.addEventListener("click", outsideDelete);
+
+  // open delete modal
+  function openDelete() {
+    confirmBox.style.display = "flex";
+  }
+  
+  // delete article
+  function deleteArticle() {
+    document.querySelector(".hero-content-article").outerHTML = "";
+    closeDeleteModal();
+  }
+
+  // cancel delete
+  function cancelDelete() {
+    closeDeleteModal();
+  }
+
+  // close delete modal
+  function closeDeleteModal() {
+    confirmBox.style.display = "none";
+  }
+
+  // Close If Outside Click
+  function outsideDelete(e) {
+    if (e.target == confirmBox) {
+      confirmBox.style.display = "none";
+    }
+  }
+};
