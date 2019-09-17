c60ccb182f92db00e3c300de33fe840199535e30
banobepascal <banobepascal@gmail.com>
2019-09-17 12:11:39 +0300

    finished failing test for user signup

--- test/first.test.js
+++ test/first.test.js
@@ -1,12 +1,30 @@
 /* eslint-disable no-undef */
-import { assert } from 'chai';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
 
-describe('Basic mocha string test', () => {
-  it('should return number of characters in a string', () => {
-    assert.equal('hello'.length, 5);
-  });
+chai.use(chaiHttp);
+chai.should();
 
-  it('should return first character of the string', () => {
-    assert.equal('hello'.charAt(0), 'h');
+describe('Create user', () => {
+  describe('POST /', () => {
+    it('Should create user and register to the API', (done) => {
+      chai.request(app)
+        .post('api/v1/auth/signup')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+
+    // fails to register user
+    it('Should reject user incase of error', (done) => {
+      chai.request(app)
+        .post('api/v1/auth/signup')
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
   });
 });
