313c7cddeb887586df80744a11d3012027af22e7
banobepascal <banobepascal@gmail.com>
2019-09-18 23:26:12 +0300

    Created middleware and tested post /article with token

--- api/index.js
+++ api/index.js
@@ -3,12 +3,13 @@ import express from 'express';
 import signin from './Auth/signin';
 import signup from './Auth/signup';
 import articlePost from './controllers/articles';
+import auth from './middleware/auth';
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/v1/auth/signup', signup);
 app.use('/api/v1/auth/signin', signin);
-app.use('/api/v1/articles', articlePost);
+app.use('/api/v1/articles', auth, articlePost);
 
 export default app;
--- test/article.test.js
+++ test/article.test.js
@@ -1,17 +1,28 @@
 /* eslint-disable no-undef */
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import jwt from 'jsonwebtoken';
 import app from '../api/index';
 import util from './utils/util';
 
+require('dotenv').config();
+
 chai.use(chaiHttp);
 chai.expect();
+
+const payload = {
+  email: 'johndoe@test.com',
+  password: 'johndoetest',
+};
+
+const token = jwt.sign(payload, process.env.JWT_KEY);
 
 describe('Creating Article', () => {
   describe('POST /api/v1/articles', () => {
     it('should create and post article', (done) => {
       chai.request(app)
         .post('/api/v1/articles')
+        .set('x-auth-token', token)
         .send(util.article)
         .end((err, res) => {
           expect(res.body.status).to.equals(200);
@@ -25,6 +36,7 @@ describe('Creating Article', () => {
     it('should not post unsatisfied article', (done) => {
       chai.request(app)
         .post('/api/v1/articles')
+        .set('x-auth-token', token)
         .send(util.badArticle)
         .end((err, res) => {
           res.should.have.status(400);
