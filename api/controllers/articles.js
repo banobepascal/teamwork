f10558c9e253b393dff866d83995cee2a7dcc2a2
banobepascal <banobepascal@gmail.com>
2019-09-20 12:29:15 +0300

    [Finishes #68590483] Finished testing and creating route POST /articles

--- api/controllers/articles.js
+++ api/controllers/articles.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable consistent-return */
 import express from 'express';
 import moment from 'moment';
@@ -12,16 +14,18 @@ articlePost.post('/', async (req, res) => {
   const { error } = validateArticle(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const article = _.pick(req.body, [
-    'title',
-    'article',
-  ]);
+  const article = {
+    id: articles.length + 1,
+    title: req.body.title,
+    article: req.body.article,
+  };
 
   articles.push(article);
   res.status(200).json({
     status: 200,
     message: 'article successfully created',
     data: {
+      id: article.id,
       createdOn: moment().format('LLL'),
       title: req.body.title,
       article: req.body.article,
--- api/models/article.js
+++ api/models/article.js
@@ -1,5 +1,6 @@
 const articles = [
   {
+    id: 1,
     createdOn: 'September 18, 2019 8:36 PM',
     title: 'Frontend',
     article: 'Do you want to be a front end developer',
